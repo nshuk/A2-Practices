4fafd858dbd62650689aa66c7f42dc0335e4c131
naqib shukri <118101646+nshuk@users.noreply.github.com>
2023-11-11 14:31:43 +0800

    Update 2023.10.23 - Binary Search.py

--- 2023.10.23 - Binary Search.py
+++ 2023.10.23 - Binary Search.py
@@ -1,23 +1,28 @@
 myList = [16, 19, 21, 27, 36, 42, 55, 67, 76, 89]
 
-lBound = 1
-uBound = len(myList)1
+lBound = 0
+uBound = len(myList)
 index = lBound
 foundFlag = False
 searchValue = int(input("Please enter an integer: "))
 
 while (foundFlag == False) and (lBound <= uBound):
-    index = int((lBound+uBound)/2)
+    index = int((lBound+uBound)/2) # ensure the use of int because index are whole numbers only
     if myList[index] == searchValue:
         foundFlag = True
         foundIndex = index
-    elif myList[index] < searchValue:
-        lBound = index + 1
+    elif myList[index] < searchValue: # if item in middle array < the search item
+        lBound = index + 1 # go right. meaning lbound is shifted to the right
     else:
-        uBound = index - 1
+        uBound = index - 1 # go left. meaning ubound is shifted to the left
 
 if foundFlag == True:
     print("The value is found at", foundIndex)
 else:
     print("the value is not found")
 
+# our index starts with 0, all the way until index 9
+# since we have 10 items in our array, the len mylist will also bring about the value 10
+# arrays of bsearch has to be pre-arranged in ascending order first 
+# binary search will start from the middle of array, then go left or right depending on the value of the search item
+# 
