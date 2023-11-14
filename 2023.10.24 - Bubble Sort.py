c87d2090dc853891b8012743fda20bcc69c2f401
naqib shukri <118101646+nshuk@users.noreply.github.com>
2023-11-14 11:02:32 +0800

    Update 2023.10.24 - Bubble Sort.py

--- 2023.10.24 - Bubble Sort.py
+++ 2023.10.24 - Bubble Sort.py
@@ -1,23 +1,24 @@
 myList = [27,19,36,42,16,89,21,16,55]
 
 lbound = 0
-uBound = 9
+uBound = 9 # can use len mylist
 n = uBound - 1
 swapFlag = False
 
 print("The unsorted array is: ", myList)
 while (swapFlag == False) or (n>0):
-    for i in range(lbound,n):
-        if myList[i] > myList[i+1]:
-            temp = myList[i]
+    for i in range(lbound,n): #start lbound, end at n-1
+        if myList[i] > myList[i+1]: #if greater
+            temp = myList[i] #swapping values
             myList[i] = myList[i+1]
             myList[i+1] = temp
-            swapFlag = True
+            swapFlag = True # raise the flag
     n = n - 1
 
 print("After ascending sorting, the array is: ", myList)
 
-#Reinitialize the changing variables
+#Reinitialize the changing variables swapflag and n has been changed
+# this one below is for sorting to descending order
 swapFlag = False
 n = uBound -1
 
