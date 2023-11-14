59ee8f664c2d0e56d6689eb4ed6b4d374737bfbf
naqib shukri <118101646+nshuk@users.noreply.github.com>
2023-11-14 11:31:45 +0800

    Update 2023.10.25 - Insertion Sort.py

--- 2023.10.25 - Insertion Sort.py
+++ 2023.10.25 - Insertion Sort.py
@@ -2,11 +2,11 @@ myList = [53,21,60,18,42,19]
 
 print("The unsorted array is: ", myList)
 
-upBound = len(myList)
+upBound = len(myList) - 1
 lowBound = 0
 
-for x in range ((lowBound +1), upBound): #start at lbound-1, end at upbound -1
-    left = x - 1 
+for x in range ((lowBound+1), (upBound+1)): #start at lbound+1, end at upbound
+    left = x - 1
     current = myList[x]
     if myList[left] > current:
         while (myList[left] > current) and (left >-1):
@@ -21,7 +21,7 @@ print ("The sorted array is: ", myList)
 # the outer loop (for x in range) iterates over each element in the list, starting from the second element (x = 1)
 # left = x - 1 initializes the variable left to the index one position before the current element
 # current = myList[x] stores the value of the current element
-# the if statement checks if the element to the left of the current element is greater than the current element. if true, it enters the nested while loop, 
+# the if statement checks if the element to the left of the current element is greater than the current element. if true, it enters the nested while loop,
 # and if it isnt true, meaning already sorted, therefore we'll go to the next value of x
 # the while loop swaps elements to the right until the correct position for the current element is found within the sorted portion of the list
-# it continues swapping until the left element is no longer greater than the current element or until it reaches the beginning of the list (left > -1).
+# it continues swapping until the left element is no longer greater than the current element or until it reaches the beginning of the list (left > -1)
